43c9ee369173249500eb14b95754e5861aac7163
David Symonds <dsymonds@golang.org>
2013-07-11 20:34:15 +1000

    Cheezy hacks for a hack night demo.

--- govent/main.go
+++ govent/main.go
@@ -6,6 +6,7 @@ package govent
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 type Interface interface {
@@ -25,5 +26,16 @@ func (s *State) Unmarshal(data []byte) error {
 
 func (s *State) Execute(cmd string) string {
 	s.Count++
+
+	// Hack night!
+	switch {
+	case cmd == "LOOK":
+		return "You are in a room full of gophers."
+	case strings.HasPrefix(cmd, "LOOK AT "):
+		return "He glares at you in return."
+	case cmd == "GET LAMP":
+		return "Okay, you have a lamp, smartass. Now what?"
+	}
+
 	return fmt.Sprintf("You said %q. That was your %dth command.", cmd, s.Count)
 }
